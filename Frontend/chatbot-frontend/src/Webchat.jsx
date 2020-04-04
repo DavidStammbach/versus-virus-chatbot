780792ff183fea2e0a3c4a88dc4809e229faa2dc
DavidStammbach <david_stammbach@bluewin.ch>
2020-04-04 16:31:36 +0200

    Deleted old folders

--- Frontend/chatbot-frontend/src/Webchat.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useMemo } from 'react';
-import ReactWebChat, { createDirectLine } from 'botframework-webchat';
-
-export default () => {
-  const directLine = useMemo(() => createDirectLine({ token: 'gBMtzerPyr0.o4xxgEoupK0Z4MCeTYCQvtBNn3Hdv0nqTmGiHWZrx0w' }), []);
-
-  return <ReactWebChat directLine={directLine} userID="YOUR_USER_ID" />;
-};
