4db86d694e39149e1008e45b34fee4b5bfcc21f4
Torben <torben.mannhart@gmail.com>
2020-04-04 19:07:21 +0200

    created chat bot

--- backend/config.py
+++ backend/config.py
@@ -18,7 +18,7 @@ class DefaultConfig:
     QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "")
     QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "")
 
-    LUIS_APP_ID = os.environ.get("LuisAppId", "")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "c9c1ffd0-f0b8-411b-b5b4-c0f1c1c32bd3")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "c2b9131e62d64e96af804634576e4d80")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "versus-virus-luis.cognitiveservices.azure.com/")
