b6282d2a3b5e0e4d9166d8c7267014a06fce00ff
DavidStammbach <david_stammbach@bluewin.ch>
2020-04-05 19:51:58 +0200

    Add build folder

---
+++ frontend/chatbot-frontend/build/precache-manifest.4e8dabf44a1cc1ae2a163ab88359729c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0137810b27cea88b0f6fdeb6372af36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344419044b13c8259f5a",
+    "url": "/static/css/main.24801ce4.chunk.css"
+  },
+  {
+    "revision": "55b5062e0dba7fd542ba",
+    "url": "/static/js/2.6a1594ce.chunk.js"
+  },
+  {
+    "revision": "5ee7a242877f91e6eba05730c88ffd10",
+    "url": "/static/js/2.6a1594ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344419044b13c8259f5a",
+    "url": "/static/js/main.796935a1.chunk.js"
+  },
+  {
+    "revision": "3750558fc2f6b427c64d",
+    "url": "/static/js/runtime-main.7bb9c619.js"
+  },
+  {
+    "revision": "77a99d28192a1b0aa8df44b9fe03b104",
+    "url": "/static/media/color.77a99d28.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
